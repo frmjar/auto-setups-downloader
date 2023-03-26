a353d78fc132f475121997f8caaaba8f19f27491
Félix Redondo <frmanzanares@hotmail.com>
2023-03-26 10:56:11 +0200

    Refactor constantes.js to use new environment variable names

--- utils/constantes.js
+++ utils/constantes.js
@@ -1,8 +1,8 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const PASS = process.env.PASSWORD || 'PASSWORD-HERE'
-const USER = process.env.USERNAME || 'USER-HERE'
+const PASS = process.env.PASSWORD_PURE || 'PASSWORD-HERE'
+const USER = process.env.USERNAME_PURE || 'USER-HERE'
 const PATH_DEFAULT = process.env.PATH_DOWNLOAD_DEFAULT || 'PATH-TO-DOWNLOAD-HERE'
 const PATH_IRACING = process.env.PATH_DOWNLOAD_IRACING || 'PATH-IRACING-HERE'
 
